8e27eed8e55e9359275bbef84f3dd724c88bae3f
supriyamulik10-web <supriyamulik10@gmail.com>
2026-08-16 13:13:29 +0530

    Add About Us component

---
+++ src/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function AboutUs() {
+    return (
+        <div className="about-us">
+            <h1>About Paradise Nursery</h1>
+
+            <p>
+                Paradise Nursery is an online plant shop that makes it easy for
+                plant lovers to discover and purchase beautiful houseplants.
+            </p>
+
+            <p>
+                We offer a variety of indoor plants across different categories,
+                carefully selected to bring freshness and natural beauty into your
+                home or workspace.
+            </p>
+
+            <p>
+                Our goal is to make plant shopping simple, convenient, and enjoyable
+                by providing useful plant information, affordable prices, and an easy
+                shopping experience.
+            </p>
+        </div>
+    );
+}
+
+export default AboutUs;
